9d3a6504dda9fa91642e681add0e394ff4289fdd
Lei Su <lei.susl@shopee.com>
2024-01-18 18:09:17 +0800

    feat: add github action

--- setenv.sh
+++ setenv.sh
@@ -2,4 +2,4 @@
 # it will be sourced in by bin/jmeter
 
 # Use a bigger heap, but a smaller metaspace, than the default
-export HEAP="-Xms8g -Xmx8g -XX:MaxMetaspaceSize=2g"
+# export HEAP="-Xms8g -Xmx8g -XX:MaxMetaspaceSize=2g"
