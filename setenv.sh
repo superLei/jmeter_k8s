d59557289840664aede37ac5f95282106170e796
Lei Su <lei.susl@shopee.com>
2024-06-26 09:50:41 +0800

    feat: add more params for dockerfile

--- setenv.sh
+++ setenv.sh
@@ -3,3 +3,4 @@
 
 # Use a bigger heap, but a smaller metaspace, than the default
 # export HEAP="-Xms8g -Xmx8g -XX:MaxMetaspaceSize=2g"
+export HEAP="-Xms2g -Xmx4g -XX:+UseG1GC -XX:MaxGCPauseMillis=200  -XX:MaxMetaspaceSize=2g"
